a1cef300b42828cf2d8376373ae9f7f997c03ec0
Dragos Dobre <dragos@bigkittylabs.com>
2023-05-15 18:29:47 +0300

    fix: navigating to wrong route on product click

--- take_a_byte/src/components/BrowseTheMenu/menu_item.js
+++ take_a_byte/src/components/BrowseTheMenu/menu_item.js
@@ -1,21 +1,19 @@
 import React from "react";
 import './css/menu_item.css'
-import testIMG from '../../assets/images/test_image.jpg'
-import {useNavigate} from "react-router-dom";
 import {Rating} from "@mui/material";
 
 function MenuItem(props) {
-    let navigate = useNavigate();
+    console.log(props);
     return (
-        <div className="div_menu_item" onClick={() => navigate("/pages/menu_item_details")}>
+        <div className="div_menu_item">
             <div className="circle_image">
-                <img className="item_image" src={props.image}/>
+                <img className="item_image" src={props.image} alt={`image-${props.title}`}/>
             </div>
             <div className="div_square_item">
                 <div className="div_menu_info">
                     <h3>{props.title}</h3>
                     <Rating name="half-rating-read" value={props.rating} precision={0.5} readOnly />
-                    <p>Cooking time: {props.time} mins</p>
+                    <p>Cooking time: {props.time} minutes</p>
                 </div>
             </div>
         </div>
